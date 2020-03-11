55eb84e6ff6f3a9392676d4332b6d1e01cd88d07
DenisStarover <58366156+DenisStarover@users.noreply.github.com>
2020-03-11 16:18:26 +0300

    Add files via upload

---
+++ BookLib/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLib
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BookCollection bookCollection = new BookCollection();
+            var c = bookCollection.GetByAuthors(new List<string> { "Толстой", "Достоевский"});
+            ShowList(c);
+            Console.ReadLine();
+        }
+        static void ShowList(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+
+    public class Book
+    {
+        public string Author;
+        public string Name;
+        public string Text;
+        public Book(string Author, string Name, string Text)
+        {
+            this.Author = Author;
+            this.Name = Name;
+            this.Text = Text;
+        }
+        public override string ToString()
+        {
+            return $"Имя: {Name}, Автор: {Author}, Текст: {Text}";
+        }
+
+    }
+
+}
---
+++ BookLib/BookCollection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLib
+{
+    interface IBookCollection
+    {
+        List<Book> GetAll();
+        Book GetByPosition(int position);
+        List<Book> GetByAuthor(string author);
+        List<Book> GetByName(string name);
+        List<Book> GetByAuthors(List<string> authors);
+        List<Book> GetByWord(string word);
+
+    }
+    public class BookCollection : IBookCollection
+    {
+        public List<Book> books = new List<Book>();
+        public BookCollection()
+        {
+            books.Add(new Book("Достоевский", "Преступление и наказание", "Убил старушку, горемыка"));
+            books.Add(new Book("Гоголь", "Мертвые души", "Мертвецы восстали"));
+            books.Add(new Book("Толстой", "Война и мир", "Была война, теперь мир"));
+            books.Add(new Book("Иванов", "Тополя", "Тополиный пух, жара, июль"));
+        }
+
+        public List<Book> GetAll()
+        {
+            return books;
+        }
+        public Book GetByPosition(int position)
+        {
+            var GetByPosition = books[position];
+            return GetByPosition;
+        }
+        public List<Book> GetByAuthor(string author)
+        {
+            var selectAuthor = books.Where(u => u.Author.Contains(author)).ToList();
+            return selectAuthor;
+        }
+
+        public List<Book> GetByName(string name)
+        {
+            var md = books.Exists(u => u.Name == name);
+            var selectName = books.Where(u => u.Name.Contains(name)).ToList();
+            return selectName; 
+        }
+
+        public List<Book> GetByAuthors(List<string> authors)
+        {
+            var selectManyAuthors = books.Where(u => authors.Contains(u.Author)).ToList();
+            return selectManyAuthors;
+        }
+
+        public List<Book> GetByWord(string word)
+        {
+            var selectByWord = books.Where(u => u.Text.Contains(word)).ToList();
+            return selectByWord;
+        }
+
+    }
+
+}
